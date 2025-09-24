5de65cfaee94b1c689cf1a37b582e6e5ddb62304
Henrique Ortiz <ortiz_henrique@hotmail.com>
2025-09-23 21:26:05 -0300

    userService (service global para o projeto todo))

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
+
+  constructor(private userService: UserService) {
+  }
 }
